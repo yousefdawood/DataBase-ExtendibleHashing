3a7ce35bbedaa41e0339e65f32f5339b77457813
YousifGamal <yousifgahmed@gmail.com>
2020-12-14 00:36:32 +0200

    merging

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 
 #include"readfile.cpp"
 #include"extendibleHashing.cpp"
+#define DATAFILESIZE HASHNUMBER*sizeof(Bucket)
+#define DIRECTORYFILESIZE (HASHNUMBER+10)*sizeof(DirectoryRecord)
 using namespace std;
 
 int main(){
+
     int fdh = createFile(DIRECTORYFILESIZE, "DirectoryFile", 0);
     int fbh = createFile(DATAFILESIZE, "DataFile", 1);
     DisplayDirectoriesFile(fdh,fbh);
